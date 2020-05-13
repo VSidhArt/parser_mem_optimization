7db5ec8e8d1651f9e5a7ed1ab45d61df76966e08
Vadim Sergeevich <vs.sidhart@gmail.com>
2020-05-13 15:19:40 +0530

    add object space bench

---
+++ object_space_bench.rb
@@ -0,0 +1,11 @@
+require './task.rb'
+def print_object_space_count_objects
+  "count_objects: #{ObjectSpace.count_objects}"
+end
+
+before_total_object = ObjectSpace.count_objects[:TOTAL]
+puts "Before: #{print_object_space_count_objects}"
+work
+after_total_object = ObjectSpace.count_objects[:TOTAL]
+puts "After: #{print_object_space_count_objects}"
+puts "Total objects diff: #{after_total_object - before_total_object}"
