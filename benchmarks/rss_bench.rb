bce2f2456f899cf5625391cf212a0111941dcbf3
Vadim Sergeevich <vs.sidhart@gmail.com>
2020-05-17 15:30:11 +0530

    add result to README

--- benchmarks/rss_bench.rb
+++ benchmarks/rss_bench.rb
@@ -4,5 +4,5 @@ require_relative '../deoptimized.rb'
 require_relative '../optimized.rb'
 
 puts "Before #{format('%d MB', (`ps -o rss= -p #{Process.pid}`.to_i / 1024))}"
-ARGV.include?('deopt') ? Deoptimized.work : Optimized.work
+ARGV.include?('deopt') ? Deoptimized.work('data/data_10_000.txt') : Optimized.work('data/data_10_000.txt')
 puts "After #{format('%d MB', (`ps -o rss= -p #{Process.pid}`.to_i / 1024))}"
--- optimized.rb
+++ optimized.rb
@@ -6,14 +6,30 @@ require 'date'
 require 'set'
 
 module Optimized
-  class User
-    attr_reader :attributes, :sessions
+  def work(file = 'data/data_100_000.txt')
+    data = parse_file(file)
+    users = data[:users]
+    sessions = data[:sessions]
 
-    def initialize(attributes:, sessions:)
-      @attributes = attributes
-      @sessions = sessions
+    report = {}
+
+    uniqueBrowsers = Set.new
+    sessions.values.flatten.each {  |s| uniqueBrowsers.add(s['browser']) }
+
+    report[:totalUsers]          = users.count
+    report[:uniqueBrowsersCount] = uniqueBrowsers.count
+    report[:totalSessions]       = data[:total_sessions]
+    report[:allBrowsers]         = uniqueBrowsers.to_a.sort!.join(',')
+    report[:usersStats] = {}
+
+    users.each do |u_id, user|
+      user_sesssions = sessions[u_id]
+      report[:usersStats][user["full_name"]] = user_stat(user_sesssions)
     end
+
+    File.write('result.json', "#{report.to_json}\n")
   end
+
 
   def parse_user(fields)
     {
@@ -28,13 +44,13 @@ module Optimized
       'user_id' => fields[1],
       'session_id' => fields[2],
       'browser' => fields[3].upcase,
-      'time' => fields[4],
+      'time' => fields[4].to_i,
       'date' => fields[5].chomp!
     }
   end
 
   def user_stat(user_sessions)
-    sessions_times = user_sessions.map { |s| s['time'].to_i }
+    sessions_times = user_sessions.map { |s| s['time'] }
     user_browsers = user_sessions.map { |s| s['browser'] }.sort
 
     {
@@ -65,30 +81,6 @@ module Optimized
     end
 
     { users: users, sessions: sessions, total_sessions: total_sessions }
-  end
-
-  def work(file = 'data/data_100_000.txt')
-    data = parse_file(file)
-    users = data[:users]
-    sessions = data[:sessions]
-
-    report = {}
-
-    uniqueBrowsers = Set.new
-    sessions.values.flatten.each {  |s| uniqueBrowsers.add(s['browser']) }
-
-    report[:totalUsers]          = users.count
-    report[:uniqueBrowsersCount] = uniqueBrowsers.count
-    report[:totalSessions]       = data[:total_sessions]
-    report[:allBrowsers]         = uniqueBrowsers.to_a.sort!.join(',')
-    report[:usersStats] = {}
-
-    users.each do |u_id, user|
-      user_sesssions = sessions[u_id]
-      report[:usersStats][user["full_name"]] = user_stat(user_sesssions)
-    end
-
-    File.write('result.json', "#{report.to_json}\n")
   end
 
   module_function :work, :parse_user, :parse_session, :user_stat, :parse_file
